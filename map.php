59b5f7e1e2737e56fc31bcb1a68732011b5f4f91
IvanMalevany <30840309+IvanMalevany@users.noreply.github.com>
2018-06-07 18:36:20 +0300

    update_L_2

---
+++ map.php
@@ -0,0 +1,410 @@
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>ICO</title>
+    <!--<script src="resources/js/jquery.js"></script>-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="/resources/animate/js/jquery.waypoints.js"></script>
+    <script src="resources/js/functions.js"></script>
+    <link rel="stylesheet" href="resources/css/fonts.css">
+    <link rel="stylesheet" href="resources/css/style.css">
+</head>
+<body>
+
+<!-- map header -->
+<div class="headerBg">
+    <div class="generalWidth">
+        <div class="mapHeaderContainer">
+            <div class="leftPart">
+                <div class="image">
+                    <img src="resources/img/logoMapHeader.png" alt="logoMapHeader"/>
+                </div>
+                <div class="text">
+                    <div class="name">
+                        Solve.Care
+                    </div>
+                    <div class="description">
+                        Это платформа, которая использует технологию блокчейн
+                        для снижения затрат в сфере здравоохранения. 
+                    </div>
+                    <div class="tagline">
+                        Energy, Agriculture, Banking, Trading
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="rightPart">
+                <div class="name">
+                    Официальные источники
+                </div>
+                <div class="location">
+                    Резиденция: Estonia: Narva mnt 5, Tallinn city,
+                    Harju county, 10117
+                </div>
+                <div class="sources">
+                    Веб-ресурсы:
+                    <a href="" class="linkProject">Whitepaper</a>
+                    <a href="" class="linkProject">Website</a>
+                </div>
+                <div class="sources">
+                    Социальные сети:
+                    <a href="" class="socialLink">
+                        <img src="resources/img/social1.png" alt="social"/>
+                    </a>
+                    <a href="" class="socialLink">
+                        <img src="resources/img/social2.png" alt="social"/>
+                    </a>
+                    <a href="" class="socialLink">
+                        <img src="resources/img/social3.png" alt="social"/>
+                    </a>
+                    <a href="" class="socialLink">
+                        <img src="resources/img/social4.png" alt="social"/>
+                    </a>
+                    <a href="" class="socialLink">
+                        <img src="resources/img/social5.png" alt="social"/>
+                    </a>
+                </div>
+                <div class="tracing">
+                    Отслеживание кода продукта:
+                    <a href="">https://github.com/SolveCare</a>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+<!-- -->
+
+<!-- menu map -->
+<div class="menuMapBg">
+    <div class="generalWidth">
+        <a href="" class="mapLink">
+            Детали
+        </a>
+        <a href="" class="mapLink">
+            Описание
+        </a>
+        <a href="" class="mapLink">
+            Риски
+        </a>
+        <a href="" class="mapLink">
+            Подробно
+        </a>
+        <a href="" class="mapLink">
+            Контакты
+        </a>
+    </div>
+</div>
+<!-- -->
+
+<!-- details -->
+<div class="detailContainer">
+    <div class="generalWidth">
+        <div>
+            <div class="name">
+                Детали проекта
+            </div>
+            <div class="favorites">
+                <span>В избранное</span>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="subname">
+            Стадия развития продукта 
+        </div>
+        <div class="text">
+            Стадия развития продукта На данный момент доступен лишь Care.
+            Wallet 1.0; расширение платформы 3.0 и расширение сообщества
+            Care.Protocol запланировано на четвертый квартал 2018 года. Разработка
+            продукта началась в первом квартале 2017 года, а к 1 кварталу 2018
+            года компания Solve.Care подписала контракт с первым коммерческим клиентом,
+            у которого имеется более чем 250 000 пользователей. На данный момент,
+            система Solve.Care не доступна для тестирования, а команда предоставляет
+            только короткое демо-видео продукта на своем сайте. Деятельность на странице
+            Github проекта началась примерно в ноябре 2017 года.
+        </div>
+    </div>
+</div>
+<!-- -->
+
+<!-- roadmap -->
+<div class="generalWidth">
+    <div class="methodologyContainer">
+        <div class="nameRoad">Roadmap</div>
+        <div class="line"></div>
+        <div class="items">
+            <div class="item">
+                <div class="nameItem">2017</div>
+                <div class="text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+            </div>
+            <div class="item">
+                <div class="nameItem">June, 2018 </div>
+                <div class="text">
+                    Skychain Alfa
+                </div>
+            </div>
+            <div class="item">
+                <div class="nameItem">December, 2018</div>
+                <div class="text">
+                    Skychain Beta
+                </div>
+            </div>
+            <div class="item">
+                <div class="nameItem">June, 2018</div>
+                <div class="text">
+                    Skychain Relese
+                </div>
+            </div>
+            <div class="item">
+                <div class="nameItem">2025</div>
+                <div class="text">
+                    Excepteur sint occaecat cupidatat non proident, sunt
+                    in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- -->
+
+<!-- description tokens -->
+<div class="generalWidth">
+    <div class="tokensDesription">
+        <div class="name">
+            Описание токена
+        </div>
+        <div>
+            <div class="item">
+                <div class="textContainer">
+                    <div class="title">
+                        Дата PRE-ICO 
+                    </div>
+                    <div class="text">
+                        15 января 2018 - 09 марта 2018
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Дата ICO
+                    </div>
+                    <div class="text">
+                        31 марта 2018 - 15 апреля 2018
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Цена токена 
+                    </div>
+                    <div class="text">
+                        Pre-Sale:1 CAN = 0.085 USD
+                        <div></div>Public-Sale:     1 CAN = 0.1 USD
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Цена токена 
+                    </div>
+                    <div class="text">
+                        Etherium
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Hard cap
+                    </div>
+                    <div class="text">
+                        35 000 000 (количество токенов)
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Soft cap
+                    </div>
+                    <div class="text">
+                        29 750 000 (количество токенов)
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Капитализация пула токенов 
+                    </div>
+                    <div class="text">
+                        1000000
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Блокчейн 
+                    </div>
+                    <div class="text">
+                        Etherium ERC-20
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="textContainer">
+                    <div class="title">
+                        Алгоритм консенсуса
+                    </div>
+                    <div class="text">
+                        Proof-of-Service
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Escrow 
+                    </div>
+                    <div class="text">
+                        35 000 000 (количество токенов), реализуется
+                        ООО Тайрелл Корпорейшн на Виргинских островах
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Валюты ICO
+                    </div>
+                    <div class="text">
+                        ETH , BTC , USD , EUR , USDT
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Bounty camping
+                    </div>
+                    <div class="text">
+                        15 января 2018 - 09 марта 2018.
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Дата  распределения токенов 
+                    </div>
+                    <div class="text">
+                        Нет данных
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Размещение на бирже 
+                    </div>
+                    <div class="text">
+                        Нет данных
+                    </div>
+                </div>
+                <div class="textContainer">
+                    <div class="title">
+                        Whitelist
+                    </div>
+                    <div class="text">
+                        N/A
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="textContainer">
+                    <div class="title">
+                        Функции токена
+                    </div>
+                    <div class="text">
+                         CAN token - это цифровой токен, который дает его владельцу право
+                        использовать платформу Solve.Care и получать доступ к размещенным
+                        на ней услугам. Токены предназначены для доступа к следующим сервисам:
+                        <ol>
+                            <li>
+                                Лицензиям платформы Solve.Care
+                            </li>
+                            <li>
+                                Услугам от Solve.Care Foundation
+                            </li>
+                            <li>
+                                Обработке нода и транзакций
+                            </li>
+                            <li>
+                                Подписка для использования Care.Wallet и Care.Cards
+                            </li>
+                            <li>
+                                Расширения платформы
+                            </li>
+                            <li>
+                                Все предложения на площадке Solve.Care, которые разрабатывает Компания и ее дочерние предприятия.
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+<!-- -->
+
+<!-- footer -->
+<div class="generalWidth">
+    <div class="footerContainer">
+        <div class="rowsInfo">
+            <div class="name">МЫ В СОЦСЕТЯХ</div>
+            <div>
+                <a href="" class="socialLink">
+                    <img src="resources/img/tel.png" alt="social"/>
+                </a>
+                <a href="" class="socialLink">
+                    <img src="resources/img/cb.png" alt="social"/>
+                </a>
+                <a href="" class="socialLink">
+                    <img src="resources/img/mm.png" alt="social"/>
+                </a>
+                <a href="" class="socialLink">
+                    <img src="resources/img/in.png" alt="social"/>
+                </a>
+            </div>
+        </div>
+        <div class="rowsInfo">
+            <a href="" class="linkAbout">Карта сайта</a><div></div>
+            <a href="" class="linkAbout">Политика конфиденциальности  </a><div></div>
+            <a href="" class="linkAbout">Условия и положения</a>
+        </div>
+        <div class="rowsInfo">
+            <div class="name">СВЯЖИТЕСЬ С НАМИ</div>
+            <div class="linkAbout">hello@icoscoring.com</div>
+            <a href="" class="linkAbout">Поддержка проекта</a>
+        </div>
+        <div class="rowsInfo">
+            <a href="" class="buttonOrange">Оставить отзыв</a>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div class="footerBg">
+    <div class="generalWidth">
+        <div class="text">
+            © «ICO Scoring – платформа оценки ICO проектов, поддерживаемая инвесторами<div></div>  и финансовыми экспертами. Все права защищены 2017»
+        </div>
+        <div class="logoContainer"></div>
+        <div class="clear"></div>
+    </div>
+</div>
+<!-- -->
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
